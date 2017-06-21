9db2865a8e1e9e09c2a8b1d9ce37786ecf608714
Vinícius Ramos <vinicius.ramos@blinks.com.br>
2017-06-21 15:44:45 -0300

    atualização de paths e veriicações

--- app.py
+++ app.py
@@ -3,23 +3,22 @@
 from requests import get
 from bs4 import BeautifulSoup
 from re import match
-from os.path import join
+from os.path import join, expanduser, exists
+from os import mkdir
 
-urls = [
-    "https://alpha.wallhaven.cc/search?q=&search_image=&categories=100&purity=100&sorting=random&order=desc&page=1",
-    "https://alpha.wallhaven.cc/search?q=&search_image=&categories=100&purity=100&sorting=random&order=desc&page=2",
-    "https://alpha.wallhaven.cc/search?q=&search_image=&categories=100&purity=100&sorting=random&order=desc&page=3",
-    "https://alpha.wallhaven.cc/search?q=&search_image=&categories=100&purity=100&sorting=random&order=desc&page=4",
-    "https://alpha.wallhaven.cc/search?q=&search_image=&categories=100&purity=100&sorting=random&order=desc&page=5",
-]
+default_url = "https://alpha.wallhaven.cc/search?q=&search_image=&categories=100&purity=100&resolutions=1920x1080&sorting=random&order=desc&page={}" 
 
-save_folder = "C:\\Users\\one\\Pictures\\wallpapers"
+save_folder = expanduser("~\\Pictures\\Wallpaper")
+if not exists:
+    print("Creating folder: {}".format(save_folder))
+    mkdir(save_folder)
+
 
 def extract(url, element, attribute):
     print ("Downloading '{}' ....".format(url))
     html = get(url)
     print("Parsing '{}'....".format(url))
-    bs = BeautifulSoup(html.text, 'lxml')
+    bs = BeautifulSoup(html.text)
     for el_a in bs.find_all(element):
         if el_a.has_attr(attribute):
             yield el_a[attribute]
@@ -34,9 +33,9 @@ def download(image_src):
     with open(save_path, "ab") as img:
         img.write(content)
 
-
 def start():    
-    for url in urls:
+    for pageNumber in range(1, 5):
+        url = default_url.format(pageNumber)
         for extracted_url in extract(url, "a", "href"):   
             if match(".*/wallpaper/.*", extracted_url):
                 images = extract(extracted_url, "img", "src")
@@ -44,7 +43,5 @@ def start():
                     if match(".*/wallpapers/full/.*", image_src):
                         download(image_src)  
                         
-
-
 if __name__ == '__main__':
     start()
